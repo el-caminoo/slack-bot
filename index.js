8b67e3c9f9feaa0fa540bf72bea852a3a7b563d3
Qalac <officialqalac@gmail.com>
2022-01-21 01:45:41 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var { PORT } = require("./utility");
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get("/", (req, res) => {
+    res.send("url is active");
+});
+
+app.post("/", (req, res) => {
+    var {name, age} = req.body;
+    res.json({"name": name, "age": age});
+});
+
+app.listen(PORT, () =>{
+    console.log(`server is up at ${PORT}`);
+});
