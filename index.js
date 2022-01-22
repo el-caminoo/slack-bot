0385f40202ec9a7020382225bb26f28e3494bed7
Qalac <officialqalac@gmail.com>
2022-01-22 07:59:26 +0000

    setup slash command

--- index.js
+++ index.js
@@ -17,64 +17,24 @@ receiver.router.use(cors());
 
 // receiver.router.post("/slack/events", index);
 
-app.command("/bot", async({ command, ack, say, respond}) => {
+app.command("/bot", async({ command, ack, say}) => {
     try {
         await ack();
-        respond("Welcome, How are you doing?");
-        say(
-            [
-                {
-                  "type": "section",
-                  "block_id": "section678",
-                  "text": {
-                    "type": "mrkdwn",
-                    "text": "Pick an item from the dropdown list"
-                  },
-                  "accessory": {
-                    "action_id": "text1234",
-                    "type": "static_select",
-                    "placeholder": {
-                      "type": "plain_text",
-                      "text": "Select an item"
-                    },
-                    "options": [
-                      {
-                        "text": {
-                          "type": "plain_text",
-                          "text": "*this is plain_text text*"
-                        },
-                        "value": "value-0"
-                      },
-                      {
-                        "text": {
-                          "type": "plain_text",
-                          "text": "*this is plain_text text*"
-                        },
-                        "value": "value-1"
-                      },
-                      {
-                        "text": {
-                          "type": "plain_text",
-                          "text": "*this is plain_text text*"
-                        },
-                        "value": "value-2"
-                      }
-                    ]
-                  }
+        say("Welcome, How are you doing?");
+        say([
+            {
+                type: "section",
+                text: {
+                    type: "mrkdwn",
+                    text: "we here"
                 }
-            ]
-            
-        );
+            }
+        ])
     }
     catch(err) {
         console.log(err);
     }
 });
-
-
-
-
-
 
 
 (async () => {
