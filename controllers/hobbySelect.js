87c8b23ac583f276aa770db32a305775651e7090
Qalac <officialqalac@gmail.com>
2022-01-22 15:54:13 +0000

    setup slash command

---
+++ controllers/hobbySelect.js
@@ -0,0 +1,71 @@
+
+var hobbySelect = async({ack, say}) => {
+    await ack();
+    say({
+        "type": "home",
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "What are your favourite hobbies?"
+                },
+                "accessory": {
+                    "type": "multi_static_select",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Select options",
+                        "emoji": true
+                    },
+                    "options": [
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Football",
+                                "emoji": true
+                            },
+                            "value": "value-0"
+                        },
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Music",
+                                "emoji": true
+                            },
+                            "value": "value-1"
+                        },
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Sleep",
+                                "emoji": true
+                            },
+                            "value": "value-2"
+                        },
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Movies",
+                                "emoji": true
+                            },
+                            "value": "value-3"
+                        },
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Basketball",
+                                "emoji": true
+                            },
+                            "value": "value-4"
+                        }
+                    ],
+                    "action_id": "multi_static_select-action"
+                }
+            }
+        ]
+    })
+};
+
+module.exports = {
+    hobbySelect: hobbySelect
+}
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var cors = require("cors");
 var { App, ExpressReceiver } = require("@slack/bolt");
 var { PORT, SIGNING_SECRET, BOT_TOKEN } = require("./keys");
-var { welcomeResponse } = require("./controllers/controllers");
+var { welcomeResponse } = require("./controllers/welcomeResponse")
+var { hobbySelect } = require("./controllers/hobbySelect");
 
 const receiver = new ExpressReceiver({
     signingSecret: SIGNING_SECRET,
@@ -17,6 +18,7 @@ receiver.router.use(cors());
 // receiver.router.post("/slack/events", index);
 
 app.command("/bot", welcomeResponse);
+app.action("static_select-action", hobbySelect);
 
 
 (async () => {
