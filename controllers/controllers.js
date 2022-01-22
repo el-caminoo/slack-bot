a805f4477fde2e626358f030cb94d289554239a0
Qalac <officialqalac@gmail.com>
2022-01-22 15:16:34 +0000

    setup slash command

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,9 +1,61 @@
 
-// var index = async (req, res) => {
-//     var result = await 1 + 1
-//     res.send(result)
-// };
+var welcomeResponse = async({ command, ack, say}) => {
+    try {
+        await ack();
+        say("Welcome. How are you doing? \n");
+        say({
+            "type": "home",
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Please select a suitable response"
+                    },
+                    "accessory": {
+                        "type": "static_select",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "Select an item",
+                            "emoji": true
+                        },
+                        "options": [
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Doing Well",
+                                    "emoji": true
+                                },
+                                "value": "value-0"
+                            },
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Neutral",
+                                    "emoji": true
+                                },
+                                "value": "value-1"
+                            },
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Feeling Lucky",
+                                    "emoji": true
+                                },
+                                "value": "value-2"
+                            }
+                        ],
+                        "action_id": "static_select-action"
+                    }
+                }
+            ]
+        })
+    }
+    catch(err) {
+        console.log(err);
+    }
+};
 
-// module.exports = {
-//     index: index
-// }
+module.exports = {
+    welcomeResponse: welcomeResponse
+}
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var cors = require("cors");
 var { App, ExpressReceiver } = require("@slack/bolt");
 var { PORT, SIGNING_SECRET, BOT_TOKEN } = require("./keys");
-var { dropdown } = require("./modals");
-// var { index } = require("./routes/routes");
+var { welcomeResponse } = require("./controllers/controllers");
 
 const receiver = new ExpressReceiver({
     signingSecret: SIGNING_SECRET,
@@ -17,62 +16,7 @@ receiver.router.use(cors());
 
 // receiver.router.post("/slack/events", index);
 
-app.command("/bot", async({ command, ack, say}) => {
-    try {
-        await ack();
-        say("Welcome. How are you doing? \n");
-        say({
-            "type": "home",
-            "blocks": [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Pick an item from the dropdown list"
-                    },
-                    "accessory": {
-                        "type": "static_select",
-                        "placeholder": {
-                            "type": "plain_text",
-                            "text": "Select an item",
-                            "emoji": true
-                        },
-                        "options": [
-                            {
-                                "text": {
-                                    "type": "plain_text",
-                                    "text": "*this is plain_text text*",
-                                    "emoji": true
-                                },
-                                "value": "value-0"
-                            },
-                            {
-                                "text": {
-                                    "type": "plain_text",
-                                    "text": "*this is plain_text text*",
-                                    "emoji": true
-                                },
-                                "value": "value-1"
-                            },
-                            {
-                                "text": {
-                                    "type": "plain_text",
-                                    "text": "*this is plain_text text*",
-                                    "emoji": true
-                                },
-                                "value": "value-2"
-                            }
-                        ],
-                        "action_id": "static_select-action"
-                    }
-                }
-            ]
-        })
-    }
-    catch(err) {
-        console.log(err);
-    }
-});
+app.command("/bot", welcomeResponse);
 
 
 (async () => {
