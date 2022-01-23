842e288e4bdc094a26877eaa26478ced4d3b9522
Qalac <officialqalac@gmail.com>
2022-01-23 01:30:45 +0000

    setup DB

--- controllers/thankYou.js
+++ controllers/thankYou.js
@@ -1,28 +1,15 @@
-var { feeling }  = require("./hobbySelect");
-var { PrismaClient } = require("@prisma/client");
+// var { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient()
-
-let hobbies = []
+// const prisma = new PrismaClient()
 
 var thankYou = async({body, ack, say}) => {
-
     await ack();
     say("Thank you");
 
-    let result = body.actions[0].selected_options
-    result.map(r => {
-        hobbies.push(r.text.text);
-    });
-
-    async () => {
-        await prisma.responses.create({
-            data: {
-                feeling: feeling,
-                favourite_hobbies: hobbies
-            }
-        });
-    }
+    // let result = body.actions[0].selected_options
+    // result.map(r => {
+    //     hobbies.push(r.text.text);
+    // });
 };
 
 module.exports = {
